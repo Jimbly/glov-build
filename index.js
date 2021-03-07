45d503b537f5e5c3928c14f3d8292e165fb8187f
Jimb Esser <wasteland@gmail.com>
2021-03-07 10:11:09 -0800

    Fix dependency warning, clean up output

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 module.exports = require('./lib/glov_build.js').create();
-module.exports.callbackify = require('./lib/util.js').callbackify;
+const util = require('./lib/util.js');
+module.exports.callbackify = util.callbackify;
+module.exports.forwardSlashes = util.forwardSlashes;
